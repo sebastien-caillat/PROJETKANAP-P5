62fbc9559f91afa4fa71b0c6b3880a349fe90fc0
Sébastien Caillat <sebastien.caillat96@gmail.com>
2022-05-19 00:30:28 +0200

    Cart Page - Test Version

---
+++ front/js/cart.js
@@ -0,0 +1,68 @@
+// let localStorageData = localStorage.getItem("product");
+// let retrieveLocalStorageData = JSON.parse(localStorageData);
+
+// for(items in product) {
+    // let cartItem = document.createElement("article");
+        // document.getElementById("cart__items").appendChild(cartItem);
+        // cartItem.classList.add("cart__item");
+
+    // let cartItemImg = document.createElement("div");
+        // cartItem.appendChild("cartItemImg");
+        // cartItemImg.classList.add("cart__item__img");
+
+        // let itemImg = document.createElement("img");
+        // cartItemImg.appendChild("itemImg");
+        // itemImg.src = ;
+        // itemImg.alt = ;
+
+    // let cartItemContent = document.createElement("div");
+        // document.getElementByClassName("cart__item").appendChild(cartItemContent);
+        // cartItemContent.classList.add("cart__item__content");
+
+        // let cartItemContentDescription = document.createElement("div");
+        // cartItemContent.appendChild("cartItemContentDescription");
+        // cartItemContentDescription.classList.add("cart__item__content__description");
+
+            // let itemName = document.createElement("h2");
+            // cartItemContentDescription.appendChild("itemName");
+            // itemName.textContent = ;
+        
+            // let itemColor = document.createElement("p");
+            // cartItemContentDescription.appendChild("itemColor");
+            // itemColor.textContent = ;
+
+            // let itemPrice = document.createElement("p");
+            // cartItemContentDescription.appendChild("itemPrice");
+            // itemPrice.textContent = ;
+
+        // let cartItemContentSettings = document.createElement("div");
+        // cartItemContent.appendChild("cartItemContentSettings");
+        // cartItemContentSettings.classList.add("cart__item__content__settings");
+
+            // let cartItemContentSettingsQuantity = document.createElement("div");
+            // cartItemContentSettings.appendChild("cartItemContentSettingsQuantity");
+            // cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
+            
+                // let itemQuantity = document.createElement("p");
+                // cardItemContentSettingsQuantity.appendChild("itemQuantity");
+                // itemQuantity.textContent = "Qté :";
+
+                // let itemQuantityNumber = document.createElement("input");
+                // cardItemContentSettingsQuantity.appendChild("itemQuantityNumber");
+                // itemQuantityNumber.classList.add("itemQuantity");
+                // itemQuantityNumber.type = "number";
+                // itemQuantityNumber.name = "itemQuantity";
+                // itemQuantityNumber.min = "1";
+                // itemQuantityNumber.max = "100";
+                // itemQuantityNumber.value = ;
+
+            // let cartItemContentSettingsDelete = document.createElement("div");
+            // cartItemContentSettings.appendChild("cartItemContentSettingsDelete");
+            // cartItemContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+                
+            //     let deleteItem = document.createElement("p");
+            //     cartItemContentSettingsDelete.appendChild("deleteItem");
+            //     deleteItem.classList.add("deleteItem");
+            //     deleteItem.textContent = "Supprimer";
+
+// }
