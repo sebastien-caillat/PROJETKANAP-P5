a821da6391c3060a3b46e3b04f43123ef284ad05
Sébastien Caillat <sebastien.caillat96@gmail.com>
2022-06-06 18:26:43 +0200

    Cart - Final

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,72 +1,76 @@
 let productInCart = JSON.parse(localStorage.getItem("product"));
 
-for(let product in productInCart) {
-    let cartItem = document.createElement("article");
-        document.getElementById("cart__items").appendChild(cartItem);
-        cartItem.classList.add("cart__item");
-        cartItem.setAttribute("data-id", productInCart[product].productId);
-        cartItem.setAttribute("data-color", productInCart[product].productColor);
+const getProductCard = () => {
+    for(let product in productInCart) {
+        let cartItem = document.createElement("article");
+            document.getElementById("cart__items").appendChild(cartItem);
+            cartItem.classList.add("cart__item");
+            cartItem.setAttribute("data-id", productInCart[product].productId);
+            cartItem.setAttribute("data-color", productInCart[product].productColor);
 
-    let cartItemImg = document.createElement("div");
-        cartItem.appendChild(cartItemImg);
-        cartItemImg.classList.add("cart__item__img");
+        let cartItemImg = document.createElement("div");
+            cartItem.appendChild(cartItemImg);
+            cartItemImg.classList.add("cart__item__img");
 
-        let itemImg = document.createElement("img");
-        cartItemImg.appendChild(itemImg);
-        itemImg.src = productInCart[product].productImg;
-        itemImg.alt = productInCart[product].productImgAlt;
+            let itemImg = document.createElement("img");
+            cartItemImg.appendChild(itemImg);
+            itemImg.src = productInCart[product].productImg;
+            itemImg.alt = productInCart[product].productImgAlt;
 
-    let cartItemContent = document.createElement("div");
-        cartItem.appendChild(cartItemContent);
-        cartItemContent.classList.add("cart__item__content");
+        let cartItemContent = document.createElement("div");
+            cartItem.appendChild(cartItemContent);
+            cartItemContent.classList.add("cart__item__content");
 
-        let cartItemContentDescription = document.createElement("div");
-        cartItemContent.appendChild(cartItemContentDescription);
-        cartItemContentDescription.classList.add("cart__item__content__description");
+            let cartItemContentDescription = document.createElement("div");
+            cartItemContent.appendChild(cartItemContentDescription);
+            cartItemContentDescription.classList.add("cart__item__content__description");
 
-            let itemName = document.createElement("h2");
-            cartItemContentDescription.appendChild(itemName);
-            itemName.textContent = productInCart[product].productName;
-        
-            let itemColor = document.createElement("p");
-            cartItemContentDescription.appendChild(itemColor);
-            itemColor.textContent = productInCart[product].productColor;
-
-            let itemPrice = document.createElement("p");
-            cartItemContentDescription.appendChild(itemPrice);
-            itemPrice.textContent = `${productInCart[product].productPrice} €`;
-
-        let cartItemContentSettings = document.createElement("div");
-        cartItemContent.appendChild(cartItemContentSettings);
-        cartItemContentSettings.classList.add("cart__item__content__settings");
-
-            let cartItemContentSettingsQuantity = document.createElement("div");
-            cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
-            cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
+                let itemName = document.createElement("h2");
+                cartItemContentDescription.appendChild(itemName);
+                itemName.textContent = productInCart[product].productName;
             
-                let itemQuantity = document.createElement("p");
-                cartItemContentSettingsQuantity.appendChild(itemQuantity);
-                itemQuantity.textContent = "Qté :";
+                let itemColor = document.createElement("p");
+                cartItemContentDescription.appendChild(itemColor);
+                itemColor.textContent = productInCart[product].productColor;
 
-                let itemQuantityNumber = document.createElement("input");
-                cartItemContentSettingsQuantity.appendChild(itemQuantityNumber);
-                itemQuantityNumber.classList.add("itemQuantity");
-                itemQuantityNumber.type = "number";
-                itemQuantityNumber.name = "itemQuantity";
-                itemQuantityNumber.min = "1";
-                itemQuantityNumber.max = "100";
-                itemQuantityNumber.value = productInCart[product].productQuantity;
+                let itemPrice = document.createElement("p");
+                cartItemContentDescription.appendChild(itemPrice);
+                itemPrice.textContent = `${productInCart[product].productPrice} €`;
 
-            let cartItemContentSettingsDelete = document.createElement("div");
-            cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
-            cartItemContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+            let cartItemContentSettings = document.createElement("div");
+            cartItemContent.appendChild(cartItemContentSettings);
+            cartItemContentSettings.classList.add("cart__item__content__settings");
+
+                let cartItemContentSettingsQuantity = document.createElement("div");
+                cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
+                cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
                 
-                let deleteItem = document.createElement("p");
-                cartItemContentSettingsDelete.appendChild(deleteItem);
-                deleteItem.classList.add("deleteItem");
-                deleteItem.textContent = "Supprimer";
-   
+                    let itemQuantity = document.createElement("p");
+                    cartItemContentSettingsQuantity.appendChild(itemQuantity);
+                    itemQuantity.textContent = "Qté :";
+
+                    let itemQuantityNumber = document.createElement("input");
+                    cartItemContentSettingsQuantity.appendChild(itemQuantityNumber);
+                    itemQuantityNumber.classList.add("itemQuantity");
+                    itemQuantityNumber.type = "number";
+                    itemQuantityNumber.name = "itemQuantity";
+                    itemQuantityNumber.min = "1";
+                    itemQuantityNumber.max = "100";
+                    itemQuantityNumber.value = productInCart[product].productQuantity;
+
+                let cartItemContentSettingsDelete = document.createElement("div");
+                cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
+                cartItemContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+                    
+                    let deleteItem = document.createElement("p");
+                    cartItemContentSettingsDelete.appendChild(deleteItem);
+                    deleteItem.classList.add("deleteItem");
+                    deleteItem.textContent = "Supprimer";
+    
+    }
 }
+
+getProductCard();
 
 const modifyQuantity = () => {
 
@@ -78,15 +82,16 @@ const modifyQuantity = () => {
 
                 event.preventDefault();
 
-                const baseQuantity = productInCart[i].productQuantity;
-                const newQuantity = modifyButton[i].valueAsNumber;
+                let baseQuantity = productInCart[i].productQuantity;
+                let newQuantity = modifyButton[i].valueAsNumber;
 
-                const productToModify = productInCart.find(element => newQuantity !== baseQuantity);
+                const productToModify = productInCart.filter(element => element.newQuantity == baseQuantity);
 
                 productToModify.productQuantity = newQuantity;
                 productInCart[i].productQuantity = productToModify.productQuantity;
 
                 localStorage.setItem("product", JSON.stringify(productInCart));
+                location.reload();
 
             })
         }
@@ -115,50 +120,30 @@ const deleteProduct = () => {
 
 deleteProduct();
 
-const modifyPrice = () => {
-    for(let i = 0; i < productInCart.length; i++) {
-         const multiplicatePriceByQuantity = parseInt(productInCart[i].productPrice) * parseInt(productInCart[i].productQuantity);
-         console.log(multiplicatePriceByQuantity);
-
-         const productPriceToModify = productInCart.find(element => parseInt(element.productQuantity) > 1)
-         productPriceToModify.productPrice = multiplicatePriceByQuantity;
-         productInCart[i].productPrice = productPriceToModify.productPrice;
-
-         localStorage.setItem("product", JSON.stringify(productInCart));
-         
-         if(modifyQuantity) {
-              // Essayer de stopper l'exécution du Javascript quand reload - Problème = le calcul s'exécute à nouveau quand on reload et donne un résultat erronné  
-         }
-    }
-}
-
-modifyPrice();
-
-
 const getTotalCount = () => {
-    const calculatingTotalPrice = [];
-    const calculatingTotalQuantity = [];
-    for (let i = 0; i < productInCart.length; i++) {
-        let productPriceInCart = productInCart[i].productPrice;
-        calculatingTotalPrice.push(productPriceInCart);
 
-        const initialValue = 0;
-        const totalPrice = calculatingTotalPrice.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-        console.log(totalPrice);
+    let productQuantity = document.getElementsByClassName("itemQuantity");
+    let quantityOfProducts = productQuantity.length;
 
-        const showPriceInCart = document.getElementById("totalPrice");
-        showPriceInCart.textContent = totalPrice;
+    totalProductQuantity = 0;
 
-        let productQuantityInCart = parseInt(productInCart[i].productQuantity);
-        calculatingTotalQuantity.push(productQuantityInCart);
-        const totalQuantity = calculatingTotalQuantity.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-        console.log(totalQuantity);
-
-        const showNumberOfArticlesInCart = document.getElementById("totalQuantity");
-        showNumberOfArticlesInCart.textContent = totalQuantity;
-
+    for (i = 0; i < quantityOfProducts; ++i) {
+        totalProductQuantity += productQuantity[i].valueAsNumber;
     }
+
+    let totalProductQuantityHtml = document.getElementById('totalQuantity');
+    totalProductQuantityHtml.textContent = totalProductQuantity;
+    console.log(totalProductQuantity);
+
+    totalPrice = 0;
+
+    for (let i = 0; i < productQuantity.length; ++i) {
+        totalPrice += (productQuantity[i].valueAsNumber * productInCart[i].productPrice);
+    }
+
+    let totalProductPrice = document.getElementById('totalPrice');
+    totalProductPrice.textContent = totalPrice;
+    console.log(totalProductPrice);
 }
 
 getTotalCount();
-
